868db879fdec953c782fd2ad4bc4bebb4b6aa411
febriadj <iamfebriadji@gmail.com>
2021-11-16 15:04:00 +0700

    :hammer: Add store provider

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
+import store from './redux/store';
 import App from './app';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.querySelector('#root'),
+);

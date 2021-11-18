07fafea6ff1a5916da6841e23db224d38c636d2c
febriadj <iamfebriadji@gmail.com>
2021-11-18 15:10:04 +0700

    :hammer: Update store reducer

--- client/redux/reducers/reducer.js
+++ client/redux/reducers/reducer.js
@@ -1,9 +1,15 @@
 const initialState = {
+  loggedIn: false,
   room: false,
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'counter/loggedIn':
+      return {
+        ...state,
+        loggedIn: action.payload.counter,
+      }
     case 'counter/roomIsOpen':
       return {
         ...state,

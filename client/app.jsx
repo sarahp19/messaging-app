1e1470d91e5933d6184376b1bcd1a234e641e790
febriadj <iamfebriadji@gmail.com>
2021-11-18 15:49:35 +0700

    :hammer: Update App

--- client/app.jsx
+++ client/app.jsx
@@ -1,23 +1,47 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import style from './styles/utils/app.css';
 import 'boxicons';
 
+import * as action from './redux/actions';
 import * as cont from './containers';
 
 function App() {
+  const props = useSelector((state) => state);
+  const dispatch = useDispatch();
+
   const [title, setTitle] = useState(document.title);
 
+  const loggedIn = () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      dispatch(action.loggedIn());
+    }
+  }
+
   useEffect(() => {
+    loggedIn();
+
     setTitle('Messaging - @febriadj');
     document.title = title;
-  }, [title]);
+  }, [
+    title,
+  ]);
 
   return (
-    <div className={style.app}>
-      <cont.main />
-      <cont.room />
+    <div
+      className={`${style.app} ${props.loggedIn ? null : style.active}`}
+    >
+      {
+        props.loggedIn ? (
+          < >
+            <cont.main />
+            <cont.room />
+          </>
+        ) : <cont.auth />
+      }
     </div>
-  );
+  )
 }
 
 export default App;

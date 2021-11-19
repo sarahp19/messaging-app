298e5a69e67dea01aa6a1896cb0d52a0d0b783f5
febriadj <iamfebriadji@gmail.com>
2021-11-19 23:21:30 +0700

    :hammer: Update User model

--- server/database/models/user.js
+++ server/database/models/user.js
@@ -15,36 +15,44 @@ exports.UserModel = model('users', new Schema({
   },
   password: {
     type: Schema.Types.String,
+    required: true,
   },
 }));
 
 exports.ProfileModel = model('profiles', new Schema({
-  userEmail: {
+  userId: {
     type: Schema.Types.String,
     required: true,
-    trim: true,
-    unique: true,
   },
   photo: {
     avatar: {
       type: Schema.Types.String,
       trim: true,
-      default: 'default-avatar.png',
+      default: 'defaultAvatar',
     },
     banner: {
       type: Schema.Types.String,
       trim: true,
-      default: 'default-banner.png',
+      default: 'defaultBanner',
     },
   },
   profileName: {
     type: Schema.Types.String,
     trim: true,
+    default: '',
   },
   bio: {
     type: Schema.Types.String,
     trim: true,
     maxlength: 200,
+    default: '',
+  },
+  phone: {
+    type: Schema.Types.Number,
+    default: 0,
+  },
+  contacts: {
+    type: Schema.Types.Array,
   },
   isActive: {
     type: Schema.Types.Boolean,

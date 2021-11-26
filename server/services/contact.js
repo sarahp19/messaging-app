8bd50a26efcc00837c7b24d7eed08f8a4479b51b
febriadj <iamfebriadji@gmail.com>
2021-11-26 22:13:08 +0700

    :hammer: Add contact service

---
+++ server/services/contact.js
@@ -0,0 +1,51 @@
+const ContactModel = require('../database/models/contact');
+
+module.exports = (io, socket) => {
+  socket.on('contact/add', async (args) => {
+    try {
+      await new ContactModel(args).save();
+
+      const data = await ContactModel.find({
+        ownerId: args.ownerId,
+      }).sort({
+        profileName: 1,
+      });
+
+      io.emit('contact/add/callback', {
+        success: true,
+        data,
+        message: null,
+      });
+    }
+    catch (error0) {
+      io.emit('contact/add/callback', {
+        success: false,
+        data: null,
+        message: error0.message,
+      });
+    }
+  });
+
+  socket.on('contact/get', async (args) => {
+    try {
+      const data = await ContactModel.find({
+        ownerId: args.ownerId,
+      }).sort({
+        profileName: 1,
+      });
+
+      io.emit('contact/get/callback', {
+        success: true,
+        data,
+        message: null,
+      });
+    }
+    catch (error0) {
+      io.emit('contact/get/callback', {
+        success: false,
+        data: null,
+        message: error0.message,
+      });
+    }
+  });
+}
--- server/services/index.js
+++ server/services/index.js
@@ -1,7 +1,9 @@
 const userService = require('./user');
+const contactService = require('./contact');
 
 function service(io, socket) {
   userService(io, socket);
+  contactService(io, socket);
 }
 
 module.exports = service;

a583769e18d3d2ad12c8f5493a5988c376179614
febriadj <iamfebriadji@gmail.com>
2021-11-28 22:15:23 +0700

    :hammer: Update index service

--- server/services/index.js
+++ server/services/index.js
@@ -1,9 +1,13 @@
 const userService = require('./user');
 const contactService = require('./contact');
+const chatService = require('./chat');
+const inboxService = require('./inbox');
 
 function service(io, socket) {
   userService(io, socket);
   contactService(io, socket);
+  chatService(io, socket);
+  inboxService(io, socket);
 }
 
 module.exports = service;

d1e63be2c504c74276801a3a1715108143c5e4b0
febriadj <iamfebriadji@gmail.com>
2021-12-03 23:51:02 +0700

    :hammer: Update socket URL protocol

--- client/helpers/socket.js
+++ client/helpers/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 const isDev = process.env.NODE_ENV === 'development';
-const origin = isDev ? 'http://localhost:8000' : '/';
+const origin = isDev ? 'ws://localhost:8000' : '/';
 
 export default io(origin);

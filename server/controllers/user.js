55a472f175512ad35d855a51822f8419c96b21f3
febriadj <iamfebriadji@gmail.com>
2021-12-05 23:56:23 +0700

    :hammer: Update user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,18 +2,65 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 const { UserModel, ProfileModel } = require('../database/models/user');
+const mail = require('../helpers/mail');
 const response = require('../helpers/response');
 
-exports.userRegister = async (req, res) => {
+exports.userRegisterStep1 = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    // password hash + gen salt 10
+
+    const byUsername = await UserModel.findOne({ username });
+    const byEmail = await UserModel.findOne({ email });
+
+    if (byUsername) {
+      const newError = {
+        message: 'Username has been used by another user',
+      }
+      throw newError;
+    }
+
+    if (byEmail) {
+      const newError = {
+        message: 'Email has been used by another user',
+      }
+      throw newError;
+    }
+
+    // password hash + gen salt
     const hashPass = await bcrypt.hash(password, await bcrypt.genSalt(10));
+    const key = Math.floor(1000 + Math.random() * 9000);
+
+    mail({ key, email, username });
+
+    response({
+      res,
+      data: {
+        username,
+        email,
+        password: hashPass,
+        key,
+      },
+    });
+  }
+  catch (error0) {
+    response({
+      success: false,
+      res,
+      statusCode: 400,
+      message: error0.message,
+    });
+  }
+}
+
+exports.userRegisterStep2 = async (req, res) => {
+  try {
+    console.log(req.body);
+    const { username, email, password } = req.body;
 
     const user = await new UserModel({
       username,
       email,
-      password: hashPass,
+      password,
     }).save();
 
     const profile = await new ProfileModel({
@@ -23,8 +70,9 @@ exports.userRegister = async (req, res) => {
       profileName: username,
     }).save();
 
+    console.log(user, profile);
     // merge 2 responses
-    const data = { ...user._doc, profile: { ...profile._doc } };
+    const data = { ...user, ...profile };
     response({ res, data });
   }
   catch (error0) {
@@ -50,7 +98,7 @@ exports.userLogin = async (req, res) => {
     // condition if user not found
     if (!user) {
       const newError = {
-        message: 'user with this username or email was not found',
+        message: 'User with this username or email was not found',
       }
       throw newError;
     }
@@ -60,7 +108,7 @@ exports.userLogin = async (req, res) => {
     // condition if user password is invalid
     if (!comparePass) {
       const newError = {
-        message: 'password does not match',
+        message: 'Password does not match',
       }
       throw newError;
     }

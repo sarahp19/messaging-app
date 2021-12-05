35efa215b754a6441f4a0a4f63be59db7cad8960
febriadj <iamfebriadji@gmail.com>
2021-12-05 14:39:41 +0700

    :hammer: Add logout confirmation box to Main components

---
+++ client/components/main/logout.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import style from '../../styles/components/main/logout.css';
+
+import * as action from '../../redux/actions';
+
+function Logout({
+  handleLogoutTabIsOpen,
+  logoutTabIsOpen,
+}) {
+  const { darkmode } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    handleLogoutTabIsOpen();
+
+    setTimeout(() => {
+      localStorage.removeItem('token');
+      dispatch(action.logout());
+    }, 800);
+  }
+
+  return (
+    <div
+      className={`
+        ${style.logout}
+        ${logoutTabIsOpen ? style.active : null}
+        ${darkmode ? style.dark : null}
+      `}
+    >
+      <div className={style['logout-wrap']}>
+        <h2 className={style.title}>Logout</h2>
+        <p>Are you sure you want to log out of this account?</p>
+        <div className={style.action}>
+          <button
+            type="button"
+            className={style.btn}
+            onClick={handleLogoutTabIsOpen}
+          >
+            <box-icon name="x" color={darkmode ? '#ffffffdd' : '#000000dd'}></box-icon>
+            <p className={style.paragraf}>No sure</p>
+          </button>
+          <button
+            type="button"
+            className={style.btn}
+            onClick={handleLogout}
+          >
+            <box-icon name="check" color={darkmode ? '#ffffffdd' : '#000000dd'}></box-icon>
+            <p className={style.paragraf}>Yes, I'm sure</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Logout;

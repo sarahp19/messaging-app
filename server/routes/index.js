678ee144b3f1ee00a0573c64bf0056828a68f175
febriadj <iamfebriadji@gmail.com>
2021-12-05 23:53:13 +0700

    :hammer: Update API routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const authenticate = require('../middleware/auth');
 const {
-  userRegister,
+  userRegisterStep1,
+  userRegisterStep2,
   userLogin,
   userFind,
 } = require('../controllers/user');
 const { getImage } = require('../controllers/file');
 
-router.post('/users/register', userRegister);
+router.post('/users/register', userRegisterStep1);
+router.post('/users/register/confirm', userRegisterStep2);
 router.post('/users/login', userLogin);
 
 router.get('/users', authenticate, userFind);

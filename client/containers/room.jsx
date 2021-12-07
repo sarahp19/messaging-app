073584729a85b375390bb89d8fe71f33bd8973c2
febriadj <iamfebriadji@gmail.com>
2021-12-07 23:52:18 +0700

    :hammer: Update room container

--- client/containers/room.jsx
+++ client/containers/room.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import style from '../styles/containers/room.css';
-
 import * as comp from '../components/room';
 
 function Room() {

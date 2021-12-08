2296a058ad1a82906fa6fc2a3e59e0029d128b67
febriadj <iamfebriadji@gmail.com>
2021-12-08 20:39:11 +0700

    :hammer: Update inbox service

--- server/services/inbox.js
+++ server/services/inbox.js
@@ -31,4 +31,40 @@ module.exports = (
       });
     }
   });
+
+  socket.on('inbox/update/archived', async (args) => {
+    try {
+      await InboxModel.updateMany(
+        { roomId: { $in: args.data } },
+        {
+          $set: {
+            archived: args.active,
+          },
+        },
+      );
+
+      const data = await InboxModel.find({
+        owners: {
+          $elemMatch: {
+            userId: args.userId,
+          },
+        },
+      }).sort({ updatedAt: -1 });
+
+      socket.emit('inbox/get/callback', {
+        success: false,
+        data,
+        message: null,
+        sound: false,
+      });
+    }
+    catch (error0) {
+      socket.emit('inbox/get/callback', {
+        success: false,
+        data: null,
+        message: error0.message,
+        sound: false,
+      });
+    }
+  });
 }

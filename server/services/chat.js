f8275465d36fa2102d99d6ece0b523cde0d4b8c3
febriadj <iamfebriadji@gmail.com>
2021-12-08 20:40:40 +0700

    :hammer: Update chat socket service

--- server/services/chat.js
+++ server/services/chat.js
@@ -10,7 +10,14 @@ module.exports = (
     const user = users.find((item) => item.socketId === args.socketId);
 
     try {
-      const data = await ChatModel.find({ roomId: args.roomId });
+      const inbox = await InboxModel.findOne({
+        $and: [
+          { owners: { $elemMatch: { userId: args.userId } } },
+          { owners: { $elemMatch: { userId: args.foreignId } } },
+        ],
+      });
+
+      const data = await ChatModel.find({ roomId: inbox.roomId });
 
       io
         .to(user.socketId)
@@ -36,6 +43,13 @@ module.exports = (
       const inbox = await InboxModel.findOne({ roomId: args.roomId });
       const foreign = users.find((item) => item.userId === args.to.userId)
 
+      const chat = await new ChatModel({
+        roomId: args.roomId,
+        userId: args.from.userId,
+        message: args.message,
+        reply: args.reply,
+      }).save();
+
       if (inbox) {
         await InboxModel.findOneAndUpdate({
           roomId: args.roomId,
@@ -44,32 +58,23 @@ module.exports = (
             lastMessage: {
               from: args.from.userId,
               text: args.message,
+              createdAt: chat.createdAt,
             },
           },
+          $inc: { total: 1 },
         });
       } else {
         await new InboxModel({
           roomId: args.roomId,
           owners: [args.from, args.to],
-          to: {
-            foreignId: args.to.userId,
-            avatar: args.to.avatar,
-            profileName: args.to.profileName,
-            username: args.to.username,
-          },
           lastMessage: {
             from: args.from.userId,
             text: args.message,
+            createdAt: chat.createdAt,
           },
+          total: 1,
         }).save()._doc;
       }
-
-      await new ChatModel({
-        roomId: args.roomId,
-        userId: args.from.userId,
-        message: args.message,
-        reply: args.reply,
-      }).save();
 
       const data = await ChatModel.find({ roomId: args.roomId });
       const inboxData = await InboxModel.find({
@@ -119,11 +124,75 @@ module.exports = (
             success: true,
             data: inboxDataForeign,
             message: null,
+            sound: true,
           });
       }
     }
     catch (error0) {
       io.to(user.socketId).emit('chat/add/callback', {
+        success: false,
+        data: null,
+        message: error0.message,
+      });
+    }
+  });
+
+  socket.on('chat/read', async (args) => {
+    try {
+      await InboxModel.findOneAndUpdate(
+        { roomId: args.roomId },
+        {
+          $set: {
+            'lastMessage.condition': 'read',
+            total: 0,
+          },
+        },
+      );
+
+      const foreign = users.find((item) => item.userId === args.foreignId);
+
+      await ChatModel.updateMany(
+        { condition: 'sent' },
+        { $set: { condition: 'read' } },
+      );
+
+      const chat = await ChatModel.find({ roomId: args.roomId });
+      const inboxForeign = await InboxModel.find({
+        owners: {
+          $elemMatch: {
+            userId: args.foreignId,
+          },
+        },
+      }).sort({ updatedAt: -1 });
+
+      const inboxUser = await InboxModel.find({
+        owners: {
+          $elemMatch: {
+            userId: args.userId,
+          },
+        },
+      }).sort({ updatedAt: -1 });
+
+      io.to(foreign.socketId).emit('chat/get/callback', {
+        success: true,
+        data: chat,
+        message: null,
+      });
+
+      io.to(foreign.socketId).emit('inbox/get/callback', {
+        success: true,
+        data: inboxForeign,
+        message: null,
+      });
+
+      socket.emit('inbox/get/callback', {
+        success: true,
+        data: inboxUser,
+        message: null,
+      });
+    }
+    catch (error0) {
+      socket.emit('chat/read/callback', {
         success: false,
         data: null,
         message: error0.message,

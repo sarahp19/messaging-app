1d9f7681365d0b59fec35d8d9821d31995916599
febriadj <iamfebriadji@gmail.com>
2021-12-08 20:38:48 +0700

    :hammer: Update index service

--- server/services/index.js
+++ server/services/index.js
@@ -3,17 +3,39 @@ const contactService = require('./contact');
 const chatService = require('./chat');
 const inboxService = require('./inbox');
 
-const users = [];
+let users = [];
 
 function service(io, socket) {
-  socket.on('user/connected', (args) => {
-    users.push(args);
+  socket.on('user/connect', (args) => {
+    try {
+      const user = users.find((item) => item.userId === args.userId);
+      if (user && user.socketId !== args.socketId) {
+        const newError = {
+          message: '',
+        }
+        throw newError;
+      }
+
+      users.push(args);
+      socket.emit('user/connect/callback', {
+        success: true,
+      });
+    }
+    catch (error0) {
+      socket.emit('user/connect/callback', {
+        success: false,
+      });
+    }
   });
 
   userService(io, socket, users);
   contactService(io, socket, users);
   chatService(io, socket, users);
   inboxService(io, socket, users);
+
+  socket.on('disconnect', () => {
+    users = users.filter((item) => item.socketId !== socket.id);
+  });
 }
 
 module.exports = service;

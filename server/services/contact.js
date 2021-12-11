0a98bdb347d0fc174de08f64636fcbc0cb568430
febriadj <iamfebriadji@gmail.com>
2021-12-11 15:32:19 +0700

    :hammer: Update contact socket service

--- server/services/contact.js
+++ server/services/contact.js
@@ -3,11 +3,8 @@ const ContactModel = require('../database/models/contact');
 module.exports = (
   io,
   socket,
-  users,
 ) => {
   socket.on('contact/add', async (args) => {
-    const user = users.find((item) => item.socketId === args.socketId);
-
     try {
       await new ContactModel(args).save();
 
@@ -17,14 +14,14 @@ module.exports = (
         profileName: 1,
       });
 
-      io.to(user.socketId).emit('contact/add/callback', {
+      socket.emit('contact/add/callback', {
         success: true,
         data,
         message: null,
       });
     }
     catch (error0) {
-      io.to(user.socketId).emit('contact/add/callback', {
+      socket.emit('contact/add/callback', {
         success: false,
         data: null,
         message: error0.message,
@@ -33,8 +30,6 @@ module.exports = (
   });
 
   socket.on('contact/get', async (args) => {
-    const user = users.find((item) => item.socketId === args.socketId);
-
     try {
       const data = await ContactModel.find({
         ownerId: args.ownerId,
@@ -42,14 +37,14 @@ module.exports = (
         profileName: 1,
       });
 
-      io.to(user.socketId).emit('contact/get/callback', {
+      socket.emit('contact/get/callback', {
         success: true,
         data,
         message: null,
       });
     }
     catch (error0) {
-      io.to(user.socketId).emit('contact/get/callback', {
+      socket.emit('contact/get/callback', {
         success: false,
         data: null,
         message: error0.message,

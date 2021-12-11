4e9d683ce635636b954d39cd6eed9ce1c5d9a029
febriadj <iamfebriadji@gmail.com>
2021-12-11 15:29:57 +0700

    :hammer: Update index socket service

--- server/services/index.js
+++ server/services/index.js
@@ -7,31 +7,17 @@ let users = [];
 
 function service(io, socket) {
   socket.on('user/connect', (args) => {
-    try {
-      const user = users.find((item) => item.userId === args.userId);
-      if (user && user.socketId !== args.socketId) {
-        const newError = {
-          message: '',
-        }
-        throw newError;
-      }
+    users.push(args);
 
-      users.push(args);
-      socket.emit('user/connect/callback', {
-        success: true,
-      });
-    }
-    catch (error0) {
-      socket.emit('user/connect/callback', {
-        success: false,
-      });
-    }
+    userService(io, socket, users);
+    contactService(io, socket, users);
+    chatService(io, socket, users);
+    inboxService(io, socket, users);
+
+    socket.emit('user/connect/callback', {
+      success: true,
+    });
   });
-
-  userService(io, socket, users);
-  contactService(io, socket, users);
-  chatService(io, socket, users);
-  inboxService(io, socket, users);
 
   socket.on('disconnect', () => {
     users = users.filter((item) => item.socketId !== socket.id);

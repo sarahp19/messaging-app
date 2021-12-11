e5791eb43ed0f290753fb47030d7f3a695ee7e5e
febriadj <iamfebriadji@gmail.com>
2021-12-11 15:30:17 +0700

    :hammer: Update inbox socket service

--- server/services/inbox.js
+++ server/services/inbox.js
@@ -3,11 +3,8 @@ const InboxModel = require('../database/models/inbox');
 module.exports = (
   io,
   socket,
-  users,
 ) => {
   socket.on('inbox/get', async (args) => {
-    const user = users.find((item) => item.socketId === args.socketId);
-
     try {
       const data = await InboxModel.find({
         owners: {
@@ -17,14 +14,14 @@ module.exports = (
         },
       }).sort({ updatedAt: -1 });
 
-      io.to(user.socketId).emit('inbox/get/callback', {
+      socket.emit('inbox/get/callback', {
         success: true,
         data,
         message: null,
       });
     }
     catch (error0) {
-      io.to(user.socketId).emit('inbox/get/callback', {
+      socket.emit('inbox/get/callback', {
         success: false,
         data: null,
         message: error0.message,
